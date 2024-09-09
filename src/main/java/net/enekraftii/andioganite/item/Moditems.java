848d22ccd4b899e6ef910d28dc8faaf3981e7c23
snowyiy <144264800+snowyiy@users.noreply.github.com>
2024-09-09 23:41:46 +0200

    .

---
+++ src/main/java/net/enekraftii/andioganite/item/Moditems.java
@@ -0,0 +1,8 @@
+package net.enekraftii.andioganite.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+
+public class Moditems {
+    public static final DeferredRegister<Item>
+}
